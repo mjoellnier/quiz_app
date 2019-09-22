6a138b1e9b6a783a9b29746e949cb3d0e10cca60
mjoellnier <other@max-wuest.de>
2019-09-22 16:13:45 +0200

    Display feedback of correct and incorrect answers

--- game.js
+++ game.js
@@ -70,7 +70,15 @@ choices.forEach(choice => {
     acceptingAnswers = false;
     const selectedChoice = event.target;
     const selectedAnswer = selectedChoice.dataset["number"];
-    getNewQuestion();
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
 
